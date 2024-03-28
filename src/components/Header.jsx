af2d73d6de67cb0fc8b0ff1f09eb7c6d12c9a7cc
Your Name <you@example.com>
2024-03-28 23:12:16 +0100

    Save after fylling Header DOM component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
+import profilePic from "../assets/profile_pic.jpg"
 
 export const Header = () => {
   return (
     <div>
-      <h2>Hi, I´m Arnau Vidal</h2>
+      <h2>Hi, I'm Arnau Vidal</h2>
       <h1>Frontend Developer</h1>
-      <img src="./assets/profil_pic.jpg" alt="profile-picuture"/>
+      <img src={profilePic} width="250" alt="profile-picuture"/>
+      <p>Arnau is a dynamic and ambitious frontend developer. Proficient in modern 
+        JavaScript and React, fueled by a relentless pursuit of knowledge and growth. 
+        With fluency in four languages and a background in architecture, can bring a unique
+         blend of creativity and technical expertise to every project.</p>
     </div>
   )
 }
