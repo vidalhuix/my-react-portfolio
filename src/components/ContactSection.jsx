12c8f494b5f35bfbf1b26088f78f5545ae9942fb
Your Name <you@example.com>
2024-04-02 19:39:30 +0200

    [13] contact section working in mobiles

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -3,17 +3,18 @@ import githubIcon from "../assets/Btn-github.svg"
 import IconLinkedin from "../assets/Btn-linkedin.svg"
 import IconStackoverflow from "../assets/Btn-stackoverflow.svg"
 import IconTwitter from "../assets/Btn-twitter.svg"
+import styles from "./ContactSection.module.css"
 
 export const ContactSection = () => {
   return (
-  <div>
+  <div className={styles.contactSection}>
     <h1 className="titleSection">Let's talk</h1>
-    <img className="profile-pic" src={profilePic} width="150" alt="profile-picuture"/>
+    <img className={styles.profilepic} src={profilePic} width="150" alt="profile-picuture"/>
     <h2>Arnau Vidal</h2>
     <h2>+46(0)063123345</h2>
     <h2>arnauvidal@gmail.com</h2>
-    <div>
-    <a href="https://github.com/vidalhuix"><img src={githubIcon} alt="github-icon"/></a>  
+    <div className={styles.contactIcons}>
+      <a href="https://github.com/vidalhuix"><img src={githubIcon} alt="github-icon"/></a>  
       <a href="https://www.linkedin.com/in/arnau-vidal-i-huix-4266a940/"><img src={IconLinkedin} alt="icon linkedin" /></a>
       <a href=""><img src={IconStackoverflow} alt="icon stackoverflow" /></a>
       <a href=""><img src={IconTwitter} alt="icon twitter" /></a>   
