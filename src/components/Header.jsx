75c90a8dbe3755bef0be1d0001ee2432c464f5bc
Your Name <you@example.com>
2024-04-03 11:31:16 +0200

    [18]styling on header and techsection working on all devices, save

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,19 @@ export const Header = () => {
   return (
     <div>
       <div className={styles.header}>
-        <img className={styles.profilepic} src={profilePic} alt="profile-picuture"/>
+        <img className="profile-pic picMobile" src={profilePic} alt="profile-picuture"/>
         <div>
-          <h2>Hi, I'm Arnau Vidal</h2>
-          <h1>Frontend Developer</h1>
+          <h2>Hi, I'm Arnau Vidal 👋</h2>
+          <h1 className={styles.profession}>Frontend Developer</h1>
         </div>
-        <p>I'm a dynamic and ambitious frontend developer. Proficient in modern 
-          JavaScript GPT and React, fueled by a relentless pursuit of knowledge and growth. 
-          With fluency in four languages and a background in architecture, can bring a unique
-          blend of creativity and technical expertise to every project.
-        </p>
+        <div className="flex-row">
+          <img className="profile-pic picOthers" src={profilePic} alt="profile-picuture"/>
+          <p>I'm a dynamic and ambitious junior frontend developer. Fluent in modern 
+            JavaScript and React, fueled by a hunger for knowledge and growth. 
+            With fluency in four languages and a background in architecture, I can bring a unique
+            blend of creativity and technical expertise to every project.
+          </p>
+        </div>
       </div>
       <BtnArrow />
     </div>
--- src/components/TechSection.jsx
+++ src/components/TechSection.jsx
@@ -2,9 +2,11 @@ import styles from "./TechSection.module.css"
 
 export const TechSection = () => {
   return (
-    <div className={styles.tech}>
-      <h1 className="titleSection">Tech</h1>
-      <p>HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js, Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming, GitHub. </p>
+    <div className="background-box" >
+      <div className={styles.tech}>
+        <h1 className="titleSection">Tech</h1>
+        <p>HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js, Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming, GitHub. </p>
+      </div>
     </div>
   )
 }
